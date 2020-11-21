81e38e8b1a0f8985e867008be052be2d65a73e4e
Inès MAGANGA <InesMGG@users.noreply.github.com>
2020-11-22 00:27:39 +0100

    Updated <3

---
+++ createTable.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS MaisonDL;
+DROP TABLE IF EXISTS Ligue;
+DROP TABLE IF EXISTS Employe;
+
+CREATE DATABASE MaisonDL;
+USE MaisonDL;
+
+CREATE TABLE Ligue
+(
+	NumL int not null,
+	NomL varchar(25),
+	constraint pk_Ligue primary key (idLigue)
+)ENGINE = INNODB;
+
+CREATE TABLE Employe 
+(
+	NumE int not null,
+	NomE varchar(25),
+	PrenomE varchar(25),
+	CourrielE varchar(25),
+	AdrueE varchar(25),
+	CPE int,
+	VilleE varchar(25),
+	DateAE date,
+	DateDE date,
+	EstRoot boolean,
+	EstAdmin boolean,
+	NumL int not null references Ligue(NumL),
+	constraint pk_Employe primary key (idEmploye)
+)ENGINE = INNODB;
+
---
+++ insertTable.sql
@@ -0,0 +1,10 @@
+INSERT INTO Ligue VALUES (7891, 'Ligue de basket');
+INSERT INTO Ligue VALUES (7892, 'Ligue de volley-ball');
+INSERT INTO Ligue VALUES (7893, 'Ligue de tennis');
+INSERT INTO Ligue VALUES (7894, 'Ligue de natation');
+
+
+INSERT INTO Employe VALUES (4201,'DOLI','Prane','doli.prane@yahoo.fr','23 Rue Auguste Perret',75013,'Paris','','','false','true',7891);
+INSERT INTO Employe VALUES (4202,'MONO','Poly','m.poly@gmail.com','75 Rue Pierre Demours',75017,'Paris','','','false','true',7892);
+INSERT INTO Employe VALUES (4203,'FLEUR','Rose','fleur.rose@yahoo.com','22 Rue des Plantes',75012,'Paris','','','false','true',7893);
+INSERT INTO Employe VALUES (4204,'STAN','Evelynn','evelynn.isthatbitch@gmail.com','179 Avenue Daumesnil',75012,'Paris','','','false','true',7894);
