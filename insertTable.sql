621174d2b4e4dcc5f69e0dc0566b5bf6deaabd38
Inès MAGANGA <InesMGG@users.noreply.github.com>
2020-11-23 11:57:32 +0100

    Update insertTable.sql

--- insertTable.sql
+++ insertTable.sql
@@ -4,6 +4,7 @@ INSERT INTO Ligue VALUES (7893, 'Ligue de tennis');
 INSERT INTO Ligue VALUES (7894, 'Ligue de natation');
 
 
+INSERT INTO Employe VALUES (0000,'root','','','',,'','','','true','false',);
 INSERT INTO Employe VALUES (4201,'DOLI','Prane','doli.prane@yahoo.fr','23 Rue Auguste Perret',75013,'Paris','','','false','true',7891);
 INSERT INTO Employe VALUES (4202,'MONO','Poly','m.poly@gmail.com','75 Rue Pierre Demours',75017,'Paris','','','false','true',7892);
 INSERT INTO Employe VALUES (4203,'FLEUR','Rose','fleur.rose@yahoo.com','22 Rue des Plantes',75012,'Paris','','','false','true',7893);
