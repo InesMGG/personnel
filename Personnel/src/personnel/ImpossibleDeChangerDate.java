3b64f4968c0f069275674937ef2e130a7bec3860
Gianni <GianniB77@users.noreply.github.com>
2020-11-30 16:46:39 +0100

    Create ImpossibleDeChangerDate.java

---
+++ Personnel/src/personnel/ImpossibleDeChangerDate.java
@@ -0,0 +1,13 @@
+package personnel;
+
+public class ImpossibleDeChangerDate extends Exception {
+
+	/**
+	 * 
+	 */
+	void ImpossibleDeChangerDate ()
+	{
+		System.out.println("La date de départ ne peux pas précéder la date d'arrivée.");
+	}
+
+}
