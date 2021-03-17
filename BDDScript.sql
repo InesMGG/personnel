944f9b34ab92e5c5324ecf27f44980fc9b4b2f6e
Inès MAGANGA <72012584+DeevaCyber@users.noreply.github.com>
2021-03-17 19:33:22 +0100

    Modification script BDD

--- BDDScript.sql
+++ BDDScript.sql
@@ -26,31 +26,21 @@ CREATE TABLE Employe
 	VilleE varchar(50) null,
 	DAE date null,
 	DDE date null,
-	EstRoot varchar(3),
-	EstAdmin varchar(3),
+	EstRoot boolean default 0,
+	EstAdmin boolean default 0,
 	NumLigue int(4) null,
 	primary key (NumE),
 	foreign key (NumLigue) references Ligue(NumL)
 )ENGINE = INNODB;
 
 
-INSERT INTO Employe VALUES (0,'root',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'oui','non', NULL);
-INSERT INTO Employe VALUES (1,'DOLI','Prane','doli.prane@yahoo.fr','23 Rue Auguste Perret',75013,'Paris',NULL,NULL,'non','oui',1);
-INSERT INTO Employe VALUES (2,'MONO','Poly','m.poly@gmail.com','75 Rue Pierre Demours',75017,'Paris',NULL,NULL,'non','oui',2);
-INSERT INTO Employe VALUES (3,'FLEUR','Rose','fleur.rose@yahoo.com','22 Rue des Plantes',75012,'Paris',NULL,NULL,'non','oui',3);
-INSERT INTO Employe VALUES (4,'STAN','Evelynn','evelynn.isthatbitch@gmail.com','179 Avenue Daumesnil',75012,'Paris',NULL,NULL,'non','oui',4);
-INSERT INTO Employe VALUES (5,'ROBSON','Aleksander','robson.aleksander@hotmail.com','70 Rue Stephenson',75018,'Paris',NULL,NULL,'non','non',1);
-INSERT INTO Employe VALUES (6,'FREEMAN','Kiya','k.freeman@gmail.com','244 Rue Championnet',75018,'Paris',NULL,NULL,'non','non',1);
-INSERT INTO Employe VALUES (7,'FAULKNER','Luca','luca.faulkner@yahoo.fr','37 Rue du Louvre',75002,'Paris',NULL,NULL,'non','non',1);
-INSERT INTO Employe VALUES (8,'FERGUSON','Nafeesa','ferguson.nafeesa@gmail.com','19 Rue de la Monnaie',75001,'Paris',NULL,NULL,'non','non',2);
-INSERT INTO Employe VALUES (9,'HARPER','Lennon','lennonharper@gmail.com','10 Rue Tronchet',75008,'Paris',NULL,NULL,'non','non',2);
-INSERT INTO Employe VALUES (10,'STUART','Damian','d.stuart@yahoo.com','7 Place Violet',75015,'Paris',NULL,NULL,'non','non',2);
-INSERT INTO Employe VALUES (11,'SOLOMON','Regan','regan.solomon@gmail.com','12 Rue Albert de Lapparent',75007,'Paris',NULL,NULL,'non','non',2);
-INSERT INTO Employe VALUES (12,'FRIEDMAN','Allan','a.friedman@yahoo.fr','147 Rue de Grenelle',75007,'Paris',NULL,NULL,'non','non',3);
-INSERT INTO Employe VALUES (13,'HOWARD','Alara','howard.alara@gmail.com','44 Rue Bayard',75008,'Paris',NULL,NULL,'non','non',3);
-INSERT INTO Employe VALUES (14,'SAMPSON','Cameron','c.sampson@gmail.com','1 Rue Puvis de Chavannes',75017,'Paris',NULL,NULL,'non','non',3);
-INSERT INTO Employe VALUES (15,'HAAS','Torin','haas.torin@gmail.com','Rue du Japon',75020,'Paris',NULL,NULL,'non','non',3);
-INSERT INTO Employe VALUES (16,'LEBLANC','Kiki','kiki.leblanc@gmail.com','16 Avenue de Bouvines',75011,'Paris',NULL,NULL,'non','non',4);
-INSERT INTO Employe VALUES (17,'GRAVES','Luka','l.graves@yahoo.fr','4 Rue Jean-Pierre Timbaud',75011,'Paris',NULL,NULL,'non','non',4);
-INSERT INTO Employe VALUES (18,'WOODCOCK','Marshall','marshall.woodcock@gmail.com','5 Rue Alibert',75010,'Paris',NULL,NULL,'non','non',4);
-INSERT INTO Employe VALUES (19,'BEGUM','Rhiannan','r.begum@yahoo.fr','Rue de Clignancourt',75018,'Paris',NULL,NULL,'non','non',4);
+INSERT INTO Employe VALUES (0,'root',NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,0, NULL);
+INSERT INTO Employe VALUES (1,'DOLI','Prane','doli.prane@yahoo.fr','23 Rue Auguste Perret',75013,'Paris',NULL,NULL,0,1,1);
+INSERT INTO Employe VALUES (2,'MONO','Poly','m.poly@gmail.com','75 Rue Pierre Demours',75017,'Paris',NULL,NULL,0,1,2);
+INSERT INTO Employe VALUES (3,'FLEUR','Rose','fleur.rose@yahoo.com','22 Rue des Plantes',75012,'Paris',NULL,NULL,0,1,3);
+INSERT INTO Employe VALUES (4,'STAN','Evelynn','evelynn.isthatbitch@gmail.com','179 Avenue Daumesnil',75012,'Paris',NULL,NULL,0,1,4);
+INSERT INTO Employe VALUES (5,'ROBSON','Aleksander','robson.aleksander@hotmail.com','70 Rue Stephenson',75018,'Paris',NULL,NULL,0,0,1);
+INSERT INTO Employe VALUES (6,'FREEMAN','Kiya','k.freeman@gmail.com','244 Rue Championnet',75018,'Paris',NULL,NULL,0,0,1);
+INSERT INTO Employe VALUES (7,'FAULKNER','Luca','luca.faulkner@yahoo.fr','37 Rue du Louvre',75002,'Paris',NULL,NULL,0,0,1);
+INSERT INTO Employe VALUES (8,'FERGUSON','Nafeesa','ferguson.nafeesa@gmail.com','19 Rue de la Monnaie',75001,'Paris',NULL,NULL,0,0,2);
+INSERT INTO Employe VALUES (9,'HARPER','Lennon','lennonharper@gmail.com','10 Rue Tronchet',75008,'Paris',NULL,NULL,0,0,2);
