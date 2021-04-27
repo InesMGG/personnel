90ae88a3caebbf296ed5720ada5b7b0d734728c2
Inès MAGANGA <72012584+DeevaCyber@users.noreply.github.com>
2021-04-27 23:52:33 +0200

    Création de l'interface
    Ok base de l'interface.
    Finir les fonctionnalités

--- Personnel/src/commandLine/LigueConsole.java
+++ Personnel/src/commandLine/LigueConsole.java
@@ -2,28 +2,144 @@ package commandLine;
 
 import static commandLineMenus.rendering.examples.util.InOut.getString;
 
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
 import commandLineMenus.List;
 import commandLineMenus.Menu;
 import commandLineMenus.Option;
+import personnel.Employe;
+import personnel.GestionPersonnel;
+import personnel.Ligue;
+import personnel.SauvegardeImpossible;
 
-import personnel.*;
 
-
-public class LigueConsole 
+public class LigueConsole extends JFrame
 {
 	private GestionPersonnel gestionPersonnel;
 	private EmployeConsole employeConsole;
+	private JPanel Ligue = new JPanel();
+	private JPanel Tableau= new JPanel();
+	private JPanel AjoutEmploye = new JPanel();
+	private JPanel colonne= new JPanel();
+	private JButton ajoutEmploye = new JButton("Ajouter un employe");
+	private JTable tableau;
+	private JTextField nomLigue = new JTextField();
+	private JButton modifierNomLigue = new JButton("Modifier le nom de la ligue");
+	private JButton modifierEmploye = new JButton("Modifier l'employe");
+	private JButton supprimerLigue = new JButton("Supprimer ");
 
-	public LigueConsole(GestionPersonnel gestionPersonnel, EmployeConsole employeConsole)
+	public LigueConsole(GestionPersonnel gestionPersonnel, EmployeConsole employeConsole, Employe admin) throws SauvegardeImpossible, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
 	{
 		this.gestionPersonnel = gestionPersonnel;
 		this.employeConsole = employeConsole;
+		setLookComponent(nomLigue);
+		setLookComponent(modifierEmploye);
+		setLookComponent(modifierNomLigue);
+		setLookComponent(ajoutEmploye);
+		Ligue.setLayout(new BoxLayout(Ligue, BoxLayout.LINE_AXIS));
+		Tableau.setLayout(new BoxLayout(Tableau, BoxLayout.LINE_AXIS));
+		AjoutEmploye.setLayout(new BoxLayout(AjoutEmploye, BoxLayout.LINE_AXIS));
+		colonne.setLayout(new BoxLayout(colonne, BoxLayout.PAGE_AXIS));
+		if(admin != null)
+		{
+			if(admin.estRoot())
+			{
+				JPanel ligneTableauEnTete = new JPanel();
+				ligneTableauEnTete.setLayout(new BoxLayout(ligneTableauEnTete, BoxLayout.LINE_AXIS));
+				JLabel lesLigues = new JLabel("Le tableau des ligues");
+				Ligue.add(lesLigues);
+				String colonnes[] = {"Nom ligue", "Voir ligue", "Supprimer ligue"};
+				Object[][] donnees = {};
+				DefaultTableModel ligneTable = new DefaultTableModel(donnees, colonnes);
+				for(Ligue ligues : gestionPersonnel.getLigues())
+				{
+					supprimerLigue.setText(supprimerLigue.getText() +ligues.getNom());
+					ligneTable.addRow(new Object[] {ligues.getNom(), new JButton("Voir "+ ligues.getNom()) ,supprimerLigue });
+				}
+				tableau= new JTable(ligneTable);
+				tableau.getColumn("Voir ligue").setCellRenderer(new TableComponent());
+				tableau.getColumn("Supprimer ligue").setCellRenderer(new TableComponent());
+				tableau.getColumn("Supprimer ligue").setCellEditor(new ButtonEditor(new JCheckBox()));
+				ligneTableauEnTete.add(tableau.getTableHeader());
+				tableau.setVisible(true);
+				Tableau.add(tableau);
+				colonne.add(Ligue);
+				colonne.add(ligneTableauEnTete);
+				colonne.add(Tableau);
+			}
+			else
+			{
+				nomLigue.setText(gestionPersonnel.getLigue(admin).getNom());
+				modifierNomLigue.setVisible(true);
+				modifierNomLigue.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						int erreurNomLigueExistant = 0;
+						JOptionPane fenetreSaisieNomLigue = new JOptionPane();
+						String nouveauNomLigue = fenetreSaisieNomLigue.showInputDialog(null,"Veuillez saisir un nouveau nom de ligue :","Nouveau nom ligue pour " + nomLigue.getText(),JOptionPane.QUESTION_MESSAGE);
+						for(Ligue nomLigueExistant : gestionPersonnel.getLigues())
+						{
+							if(nomLigueExistant.equals(nouveauNomLigue) || nouveauNomLigue.length() < 1)
+								erreurNomLigueExistant ++;
+						}
+						if(erreurNomLigueExistant > 0) {
+							JOptionPane fenetreErreurSaisieNomLigue = new JOptionPane();
+							fenetreErreurSaisieNomLigue.showMessageDialog(null, "Erreur saisie Ligue","Le nom de ligue saisie est incorrecte ou déjà existant.", JOptionPane.ERROR_MESSAGE);
+						}
+						else
+						{
+							try {
+								admin.getLigue().setNom(nouveauNomLigue);
+							} catch (SauvegardeImpossible e) {
+								e.printStackTrace();
+							}
+							JOptionPane fenetreSaisieNomLigueCorrecte = new JOptionPane();
+							fenetreSaisieNomLigueCorrecte.showMessageDialog(null,"Nom ligue changé", "Le nouveau nom de la ligue est : " + nouveauNomLigue, JOptionPane.INFORMATION_MESSAGE);
+						}
+					}
+				});
+				Ligue.add(nomLigue);
+				Ligue.add(modifierNomLigue);
+				colonne.add(Ligue);
+				
+			}
+			this.getContentPane().add(colonne);
+			this.setVisible(false);
+		}
 	}
 
+	public void setLookComponent(Component component) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+	{
+		UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		SwingUtilities.updateComponentTreeUI(component);
+	}
+
+	public Component getColonne() {
+		return colonne;
+	}
+	
 	Menu menuLigues()
 	{
 		Menu menu = new Menu("Gérer les ligues", "l");
@@ -73,10 +189,9 @@ public class LigueConsole
 	{
 		Menu menu = new Menu("Editer " + ligue.getNom());
 		menu.add(afficher(ligue));
-		menu.add(gererEmployes(ligue));
-		menu.add(changerAdministrateur(ligue));
 		menu.add(changerNom(ligue));
 		menu.add(supprimer(ligue));
+		menu.add(gererEmployes(ligue));
 		menu.addBack("q");
 		return menu;
 	}
@@ -87,7 +202,7 @@ public class LigueConsole
 				() -> {try {
 					ligue.setNom(getString("Nouveau nom : "));
 				} catch (SauvegardeImpossible e) {
-					System.out.println("Impossible de changer le nom de la ligue");
+					e.printStackTrace();
 				}});
 	}
 
@@ -106,10 +221,13 @@ public class LigueConsole
 				{
 					try {
 						ligue.addEmploye(getString("nom : "), 
-							getString("prenom : "), getString("mail : "), 
-							getString("password : "), LocalDate.parse(getString("date d'arrivée : ")), LocalDate.parse(getString("date départ : ")));
-					} catch (SauvegardeImpossible e) {
-						System.out.println("Impossible de sauvegarder cette employé");
+								getString("prenom : "), getString("mail : "), 
+								getString("password : "), 
+								LocalDate.parse(getString("date de départ (AAAA-MM-JJ) : ")), LocalDate.parse(getString("date d'arrivé  (AAAA-MM-JJ) : ")));
+					} 
+					catch (Exception e) {
+						System.out.println("erreur de format dans l'insertion de la date");
+						e.printStackTrace();
 					}
 				}
 		);
@@ -121,61 +239,94 @@ public class LigueConsole
 		menu.add(afficherEmployes(ligue));
 		menu.add(ajouterEmploye(ligue));
 		menu.add(selectionnerEmploye(ligue));
-		//menu.add(modifierEmploye(ligue));
-		//menu.add(supprimerEmploye(ligue));
+		menu.add(changerAdministrateur(ligue));
 		menu.addBack("q");
 		return menu;
 	}
-
+	
 	private List<Employe> selectionnerEmploye(final Ligue ligue)
 	{
-		return new List<Employe>("Sélectionner un employé", "e",
-				()-> new ArrayList<>(ligue.getEmployes()),
-				employeConsole.editerEmploye()
-				);
-	}
-	
-	
-	
-//	private List<Employe> supprimerEmploye(final Ligue ligue)
-//	{
-//		return new List<>("Supprimer un employé", "s", 
-//				() -> new ArrayList<>(ligue.getEmployes()),
-//				(index, element) -> {element.remove();}
-//				);
-//	}
-	
-	private List<Employe> changerAdministrateur(final Ligue ligue)
-	{
-		return new List<Employe> ("Sélectionner un nouvel administrateur", "w",
-		()-> new ArrayList<>(ligue.getEmployes()),
-		(index, element)-> {
-			try {
-				ligue.setAdministrateur(element);
-			} catch (SauvegardeImpossible e) {
-				System.out.println("Impossible de changer l'administrateur de cette ligue");
-			} catch (SQLException e) {
-				System.out.println("Impossible de changer l'administrateur de cette ligue");
-			}
-		}
-		);
-	}
-	
-	private List<Employe> modifierEmploye(final Ligue ligue)
-	{
-		return new List<>("Modifier un employé", "e", 
+		return new List<>("Selectionne un employe", "e", 
 				() -> new ArrayList<>(ligue.getEmployes()),
 				employeConsole.editerEmploye()
 				);
 	}
+
+	private List<Employe> changerAdministrateur(final Ligue ligue)
+	{
+		return new List<>("Convetir employé en admin", "f", 
+				() -> new ArrayList<>(ligue.getEmployes()),
+				(index, element) -> {try {
+					ligue.setAdministrateur(element);
+				} catch (SauvegardeImpossible | SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				};
+				}
+				);
+	}		
 	
 	private Option supprimer(Ligue ligue)
 	{
 		return new Option("Supprimer", "d", () -> {try {
 			ligue.remove();
 		} catch (SauvegardeImpossible e) {
-			System.out.println("Impossible de supprimer cette ligue");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}});
 	}
 	
+	class TableComponent extends DefaultTableCellRenderer{
+
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int rown, int column)
+		{
+			if(value instanceof JButton)
+				return (JButton) value;
+			else
+				return this;
+		}
+	}
+	
+	class ButtonEditor extends DefaultCellEditor{
+		protected JButton bouton;
+		private boolean isPushed;
+		private ButtonListener bListener = new ButtonListener();
+		public ButtonEditor(JCheckBox checkBox) {
+			super(checkBox);
+			bouton = new JButton();
+			bouton.setOpaque(true);
+			bouton.addActionListener(bListener);
+		}
+		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+			bListener.setRow(row);
+			bListener.setColumn(column);
+			bListener.setTable(table);
+			bouton.setText((value == null) ? "" : value.toString());
+			return bouton;
+		}
+		class ButtonListener extends Thread implements ActionListener{
+			private int column, row;
+			private JTable table;
+			private int nbre = 0;
+			Thread thread;
+			public void setColumn(int col) {this.column = col;}
+			public void setRow(int row) {this.row = row;}
+			public void setTable(JTable table) {this.table = table;}
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				for(Ligue ligue : gestionPersonnel.getLigues())
+				{
+					if(table.getValueAt(this.row, (this.column - 2)).equals(ligue.getNom()) && ligue != null ) {
+						try {
+							ligue.remove();
+							DefaultTableModel ligneARetirer = (DefaultTableModel) table.getModel();
+							ligneARetirer.removeRow(this.row);
+						} catch (SauvegardeImpossible e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+	}
 }
--- Personnel/src/commandLine/PersonnelConsole.java
+++ Personnel/src/commandLine/PersonnelConsole.java
@@ -3,27 +3,164 @@ package commandLine;
 import personnel.*;
 import commandLineMenus.*;
 import commandLineMenus.Action;
+import commandLineMenus.Menu;
+import commandLineMenus.Option;
+import personnel.GestionPersonnel;
+import personnel.Ligue;
+import personnel.SauvegardeImpossible;
 
 import static commandLineMenus.rendering.examples.util.InOut.*;
+import static commandLineMenus.rendering.examples.util.InOut.getString;
 
+import java.sql.SQLException;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-import javax.swing.*;
-public class PersonnelConsole
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+
+
+public class PersonnelConsole extends JFrame
 {
 	private GestionPersonnel gestionPersonnel;
 	LigueConsole ligueConsole;
 	EmployeConsole employeConsole;
+	private JPanel Titre = new JPanel();
+	private JPanel Identifiant = new JPanel();
+	private JPanel MotDePasse = new JPanel();
+	private JPanel Confirmer = new JPanel();
+	private JPanel colonne= new JPanel();
+	private JButton confirmer = new JButton("Confirmer");
+	private JTextField username = new JTextField();
+	private JPasswordField motDePasse = new JPasswordField();
+	private JLabel erreurConnexion = new JLabel("Erreur lors de l'identification");
 	
-	public PersonnelConsole(GestionPersonnel gestionPersonnel)
+	public PersonnelConsole(GestionPersonnel gestionPersonnel) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, SauvegardeImpossible, SQLException
 	{
 		this.gestionPersonnel = gestionPersonnel;
 		this.employeConsole = new EmployeConsole();
-		this.ligueConsole = new LigueConsole(gestionPersonnel, employeConsole);
+		this.setTitle("Maison des ligues : Gestion des Ligues");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBackground(Color.WHITE);
+		this.setLocationRelativeTo(null);
+		setLookComponent(confirmer);
+		setLookComponent(username);
+		setLookComponent(motDePasse);
+		setLookComponent(erreurConnexion);
+		Titre.setLayout(new BoxLayout(Titre, BoxLayout.LINE_AXIS));
+		Identifiant.setLayout(new BoxLayout(Identifiant, BoxLayout.LINE_AXIS));
+		MotDePasse.setLayout(new BoxLayout(MotDePasse, BoxLayout.LINE_AXIS));
+		Confirmer.setLayout(new BoxLayout(Confirmer, BoxLayout.LINE_AXIS));
+		colonne.setLayout(new BoxLayout(colonne, BoxLayout.PAGE_AXIS));
+		Titre.add(new JLabel("Veuillez vous identifier "));
+		Identifiant.add(new JLabel("Identifiant :"));
+		username.setMinimumSize(new Dimension(150,25));
+		username.setMaximumSize(new Dimension(150,25));
+		Identifiant.add(username);
+		MotDePasse.add(new JLabel("Mot de passe :"));
+		motDePasse.setMinimumSize(new Dimension(150,25));
+		motDePasse.setMaximumSize(new Dimension(150,25));
+		MotDePasse.add(motDePasse);
 		
+		confirmer.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				try {
+					for(Ligue ligue: GestionPersonnel.getGestionPersonnel().getLigues())	
+					{
+						if(ligue.getAdministrateur().getMail().equals(username.getText().trim()) && ligue.getAdministrateur().getPassword().equals(motDePasse.getText().trim()))
+						{
+							LigueConsole ligueConsole;
+							try {
+								ligueConsole = new LigueConsole(gestionPersonnel, employeConsole,ligue.getAdministrateur());
+								colonne.removeAll();
+								colonne.add(ligueConsole.getColonne());
+								pack();
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				} catch (SauvegardeImpossible e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				try {
+					if(GestionPersonnel.getGestionPersonnel().getRoot().getNom().equals(username.getText().trim()) && GestionPersonnel.getGestionPersonnel().getRoot().getPassword().equals(motDePasse.getText().trim()))
+					{
+						LigueConsole ligueConsole;
+						try {
+							if(colonne.getComponentCount() > 4)
+								colonne.remove(erreurConnexion);
+							ligueConsole = new LigueConsole(gestionPersonnel, employeConsole,gestionPersonnel.getRoot());
+							colonne.removeAll();
+							colonne.add(ligueConsole.getColonne());
+							pack();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+					else
+						colonne.add(erreurConnexion);
+				} catch (SauvegardeImpossible e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		Confirmer.add(confirmer);
+		colonne.add(Titre);
+		colonne.add(Identifiant);
+		colonne.add(MotDePasse);
+		colonne.add(Confirmer);
+		this.getContentPane().add(colonne);
+		this.pack();
+		this.setVisible(true);
+	}
+	
+	public void setLookComponent(Component component) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+	{
+		UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		SwingUtilities.updateComponentTreeUI(component);
 	}
 	
 	public void start()
@@ -36,22 +173,24 @@ public class PersonnelConsole
 		Menu menu = new Menu("Gestion du personnel des ligues");
 		menu.add(employeConsole.editerEmploye(gestionPersonnel.getRoot()));
 		menu.add(ligueConsole.menuLigues());
-		menu.add(menuQuitter());
+		menu.add(quitterEtEnregistrer());
+		// menu.add(menuQuitter());
 		return menu;
 	}
 
-	private Menu menuQuitter()
-	{
-		Menu menu = new Menu("Quitter", "q");
-		menu.add(quitterEtEnregistrer());
-		menu.add(quitterSansEnregistrer());
-		menu.addBack("r");
-		return menu;
-	}
+//	private Menu menuQuitter()
+//	{
+//		Menu menu = new Menu("Quitter", "q");
+//		menu.add(quitterEtEnregistrer());
+//		menu.add(quitterSansEnregistrer());
+//		menu.addBack("r");
+//		return menu;
+//	}
 	
 	private Option quitterEtEnregistrer()
 	{
-		return new Option("Quitter et enregistrer", "q", 
+		return new Option("Quitter", "q",
+		// return new Option("Quitter et enregistrer", "q", 
 				() -> 
 				{
 					try
@@ -80,9 +219,9 @@ public class PersonnelConsole
 		return ok;
 	}
 	
-	public static void main(String[] args) throws SauvegardeImpossible
+	public static void main(String[] args) throws SauvegardeImpossible, SQLException 
 	{
-		JFrame frame = new JFrame();
+		/*JFrame frame = new JFrame();
 	    JPanel panel = new JPanel();
 	    panel.setPreferredSize(new Dimension(150, 150));
 	    frame.setVisible(true);
@@ -96,13 +235,19 @@ public class PersonnelConsole
 	    JLabel label = new JLabel("Maison des ligues : Gestion des ligues");
 	    panel.add(label);
 	    label.setFont(new Font("Times New Roman", Font.BOLD, 40));
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
 		
 		
-		
-		PersonnelConsole personnelConsole = 
-				new PersonnelConsole(GestionPersonnel.getGestionPersonnel());
-		if (personnelConsole.verifiePassword())
-			personnelConsole.start();
+		try {
+			PersonnelConsole fenetre = new PersonnelConsole(GestionPersonnel.getGestionPersonnel());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+		PersonnelConsole personnelConsole;
+//		personnelConsole = new PersonnelConsole(GestionPersonnel.getGestionPersonnel());
+//		if (personnelConsole.verifiePassword())
+//			personnelConsole.start();
 	}
+	
 }
